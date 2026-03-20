ad02e98f3799f6bdde31b60202a16cb448919fcc
Nickollas09 <nickorner29@gmail.com>
2026-03-20 11:32:01 -0300

    aula 2 variaveis e operadores

---
+++ Aula 02-Variaveis-Operadores/aula2a py.py
@@ -0,0 +1,29 @@
+print('ola mundo')
+
+print(7+4)
+print('7'+'4') #CONCATENAÇÃO DE STRINGS
+print('7 + 4')
+
+#Comentario de 1 linha
+'''
+Comentario de muitas 
+linhas
+'''
+
+#VARIAVEIS
+nome = "Nickollas" # str
+idade = 18 #int
+peso = 90 #float
+
+print(nome, idade, peso)
+print(f"oiii {nome}!!!!")
+
+
+#INPUT - SIMULAÇÃO DE FORMULARIO NO CMD
+nome = input("Digite seu nome: ")
+idade = int(input("Dgite sua idade: "))
+
+nova_idade = idade + 1
+
+print(nome, idade)
+print(idade + 1)
---
+++ Aula 02-Variaveis-Operadores/dseafios.py
@@ -0,0 +1,14 @@
+# Desafio 1
+
+nome = input("Digite seu nome:")
+
+print("Desafio 1")
+print(f"Seja bem vindo {nome}")
+
+#Desafio 2
+
+dia = input("Insira o dia de seu nascimento:")
+mes = input("Insira o mes de seu nascimento:")
+ano = input("Insira o ano de seu nascimento:")
+
+print(f"Seu nascimento o {dia} do {mes} no ano {ano}")
